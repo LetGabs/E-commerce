e919cf58212113746389cd0e1eec44141ec770e7
lararaffy <lararaffaelly2005@gmail.com>
2025-09-17 16:27:45 -0300

    Dados iniciais no JS

---
+++ lara/checkout.js
@@ -0,0 +1,13 @@
+// Dados gerais com ums 'const' englobando todos os campos do formulário
+const dadosGerais = {
+    nome: document.getElementById('nome'),
+    email: document.getElementById('email'),
+
+    cep: document.getElementById('cep'),
+    rua: document.getElementById('rua'),
+    numero: document.getElementById('numero'),
+    bairro: document.getElementById('bairro'),
+    cidade: document.getElementById('cidade'),
+    uf: document.getElementById('uf'),
+}
+
